fcf1a054bfa73d488e9c95237aa54aa38e972705
Peerapat <peerapat.maidaeng@gmail.com>
2025-12-21 13:00:31 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   skipTrailingSlashRedirect: true,
   distDir: 'dist',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
     domains: ["localhost"],
