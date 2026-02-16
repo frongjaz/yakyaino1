851411091b2070ca71b09ce5724920529a14567f
Frong <peerapat.maidaeng@gmail.com>
2026-02-16 13:52:31 +0700

    chore: Remove `trailingSlash` and `skipTrailingSlashRedirect` Next.js config options.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export', // Comment out for Node.js hosting (uncomment for static export)
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
